00c31629181950030066476e20adda4258c86a5c
tornike2002 <toko.toka2000@gmail.com>
2024-10-05 23:34:36 +0400

    added public folder

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ const App = () => {
           <Route path="/stories" element={<Stories />} />
           <Route path="/services" element={<Service />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
         </Route>
+          <Route path="*" element={<NoPage />} />
       </Routes>
     </div>
   );
