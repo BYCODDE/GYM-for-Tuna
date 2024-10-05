6069814c6bba3561e1b9f03dee786c21dc16202f
tornike2002 <toko.toka2000@gmail.com>
2024-10-05 23:57:18 +0400

    added small tiny screen sizes for responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{svelte,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        tiny: { max: "300px" },
+        smaller: { min: "301px", max: "410px" },
+      },
+    },
   },
   plugins: [],
-}
+};
