d745f990ecf6da2ac4158aa2c8410988e4ad2c15
tornike2002 <toko.toka2000@gmail.com>
2024-10-06 16:51:00 +0400

    add images and implimented swiper js carousel

---
+++ src/components/home/SuccessCarousel.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+const SuccessCarousel = () => {
+  const carouselImages = [
+    "/home/mobile/nutt.png",
+    "/home/mobile/charlene.png",
+    "/home/mobile/john.png",
+    "/home/mobile/nate.png",
+    "/home/mobile/dylan.png",
+    "/home/mobile/rachel.png",
+    "/home/mobile/giuseppe.png",
+    "/home/mobile/jason.png",
+    "/home/mobile/jade.png",
+  ];
+
+  return (
+    <section className="px-[37px] font-Nunito border border-[#4D4D4D] rounded-2xl py-10 mb-10">
+      <div className="flex items-center gap-4 pb-6">
+        <img src="/icons/arrow-right.svg" alt="success-stories" className="" />
+        <h2 className="text-sm font-bold gradient-main uppercase">
+          Success Story
+        </h2>
+      </div>
+      <div>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1.3}
+        >
+          {carouselImages.map((src, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={src}
+                alt={`carousel-item-${index + 1}`}
+                className="bg-cover bg-no-repeat"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className="pt-6 flex justify-end">
+        <img src="/icons/arrow-left.svg" alt="success-stories" />
+      </div>
+    </section>
+  );
+};
+
+export default SuccessCarousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutTrainer from "../components/home/AboutTrainer";
+import SuccessCarousel from "../components/home/SuccessCarousel";
 
 const Home = () => {
   return (
     <main className="bg-primary-bg">
       <AboutTrainer />
+      <SuccessCarousel />
     </main>
   );
 };
