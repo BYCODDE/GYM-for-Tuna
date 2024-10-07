fe4587bf0c92f2a700a8ff1eed0d12d6c1e62b5e
tornike2002 <toko.toka2000@gmail.com>
2024-10-07 16:59:59 +0400

    removed unused return tags in getStory hook

--- src/hooks/useGetStory.js
+++ src/hooks/useGetStory.js
@@ -2,11 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import apiSuccessStory from "../services/apiSuccessStory";
 
 const useGetStory = () => {
-  const { data, isLoading, isError, error } = useQuery({
+  return useQuery({
     queryKey: ["storyData"],
     queryFn: apiSuccessStory,
   });
-  return { data, isLoading, isError, error };
 };
 
 export default useGetStory;
