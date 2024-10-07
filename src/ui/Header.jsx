41b78bbac854d8205e50bab91a1e62f5354a103f
BYCODDE <anaraliev217@gmail.com>
2024-10-07 14:12:28 +0400

    feat: header created!

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,21 @@
-import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-primary-bg px-[37px] py-10 font-Nunito flex justify-between items-center">
+      <div>
+        <h1 className="tiny:text-[14px] smaller:text-[20px] font-bold gradient-header uppercase text-center">
+          TRANSFORM <br /> WITH TUNA
+        </h1>
+      </div>
+      <div>
+        <img className="w-full h-full  cursor-pointer tiny:max-w-[30px] tiny:max-h-[30px]  smaller:max-w-[40px] smaller:max-h-[40px]"
+          src="\icons\burger.svg"
+          alt="burger_menu"
+        />
+
+      </div>
+    </header>
+  );
 };
 
 export default Header;
