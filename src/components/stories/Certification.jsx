2294ed0d2af60043f7cd3e2585c5cf81223eab1f
BYCODDE <anaraliev217@gmail.com>
2024-10-10 20:34:40 +0400

    style:responsive is done!

--- src/components/stories/Certification.jsx
+++ src/components/stories/Certification.jsx
@@ -12,15 +12,28 @@ function Certification() {
   }
 
   return (
-    <div className="text-white px-[37px] flex flex-col justify-center">
+    <div className=" mt-[10px] pb-10  md:max-w-[1281px] md:mx-auto 2xl:px-0   text-white px-[37px] flex flex-col justify-center">
       <div className="md:gap-[15px]  flex gap-[9px] items-center ml-[10px]">
-        <img className="md:hidden" src="/icons/arrow-right.svg" alt="arrow-right" />
-        <img  className="hidden md:block" src="/icons/three-arrow-right.svg" alt="three-arrow-right" />
-        <h3 className="md:text-[24px] text-[14px]  font-bold uppercase font-Nunito  certification-header leading-5">
-          Certification
+        <img
+          className="md:hidden"
+          src="/icons/arrow-right.svg"
+          alt="arrow-right"
+        />
+        <img
+          className="hidden md:block xl:hidden"
+          src="/icons/three-arrow-right.svg"
+          alt="three-arrow-right"
+        />
+        <img
+          className="hidden xl:block"
+          src="/icons/four-arrow-right.svg"
+          alt="four-arrow-right"
+        />
+        <h3 className="mt-[8px] text-center xl:text-[32px] text-sm font-bold uppercase leading-normal gradient-main pb-4 md:text-2xl font-Nunito">
+          certification
         </h3>
       </div>
-      <div className="md:grid  md:grid-cols-2 md:gap-4 flex flex-col mt-[30px] mb-[30px] gap-[9px]">
+      <div className="xl:grid-cols-3  md:grid  md:grid-cols-2 md:gap-4 flex flex-col mt-[30px] mb-[30px] gap-[9px]">
         {data.certification.map((data) => (
           <div key={data.id} className="flex items-center gap-4">
             <img src="/icons/certification_svg.svg" alt="certification" />
--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -9,7 +9,7 @@ const navItems = [
 ];
 const Footer = () => {
   return (
-    <footer className="md:gap-[50px]  md:justify-between md:flex-row bg-[#242424] p-[40px] flex flex-col justify-center  font-Nunito text-[#C4C4C4] h-full">
+    <footer className="xl:justify-evenly md:gap-[50px]  md:justify-between md:flex-row bg-[#242424] p-[40px] flex flex-col justify-center  font-Nunito text-[#C4C4C4] h-full">
       <div className="md:mt-[50px] flex items-center gap-4">
         <div className="flex flex-col  md:text-[24px] tiny:text-[14px] text-[20px] font-bold gradient-header uppercase text-center font-BebasNeue ">
           <span>TRANSFORM</span> <span>WITH TUNA</span>
@@ -17,21 +17,21 @@ const Footer = () => {
         <img className="" src="/icons/gym-svg.svg" alt="gym-svg" />
       </div>
       <div className="md:flex md:flex-col md:justify-center md:items-center mt-[50px]">
-        <nav className="md:gap-[21px] md:flex-row tiny:text-[12px] smaller:text-[14px] flex flex-col justify-center items-start  gap-2">
+        <nav className="xl:text-[20px] xl:gap-[41px] md:gap-[22px] md:flex-row tiny:text-[12px] smaller:text-[14px] flex flex-col justify-center items-start  gap-2">
           {navItems.map((item) => (
             <NavLink key={item.path} to={item.path} className={`text-`}>
               {item.label}
             </NavLink>
           ))}
         </nav>
-        <span className=" tiny:text-[10px] smaller:text-[12px] font-Nunito  font-normal mt-[50px]  text-center ">
+        <span className="xl:text-[20px] tiny:text-[10px] smaller:text-[12px] font-Nunito  font-normal mt-[50px]  text-center ">
           © 2024 Transform with Tuna. All rights reserved.
         </span>
       </div>
       <span className="md:hidden tiny:text-[10px] smaller:text-[12px] font-Nunito  font-normal mt-[50px]  text-center ">
         © 2024 Transform with Tuna. All rights reserved.
       </span>
-      <div className="mt-[10px] flex gap-4 w-[24px] h-[24px] flex-col ">
+      <div className="xl:mt-[50px] xl:flex-row mt-[10px] flex gap-4 w-[24px] h-[24px] flex-col ">
         <img src="/icons/facebook.svg" alt="facebook" />
         <img src="/icons/insta.svg" alt="insta" />
         <img src="/icons/tiktok.svg" alt="tiktok" />
