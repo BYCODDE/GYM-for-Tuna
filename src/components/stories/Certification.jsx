922a8e30c19ea2ca97d1f44b998f4f9d193ff5f0
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-10-10 15:07:12 +0400

    style:certification style fixed!

--- src/components/stories/Certification.jsx
+++ src/components/stories/Certification.jsx
@@ -2,7 +2,6 @@ import useGetCertification from "../../hooks/useGetCertification";
 
 function Certification() {
   const { data, isLoading, error } = useGetCertification();
-  // console.log(data.certification[0].name);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -14,22 +13,20 @@ function Certification() {
 
   return (
     <div className="text-white px-[37px] flex flex-col justify-center">
-      <div className="flex gap-4 items-center ml-[10px]">
+      <div className="flex gap-[9px] items-center ml-[10px]">
         <img src="/icons/arrow-right.svg" alt="" />
-        <h3 className="text-[20px] font-bold uppercase font-Nunito  certification-header">
+        <h3 className="tiny:text-[14px] text-[20px] font-bold uppercase font-Nunito  certification-header leading-5">
           Certification
         </h3>
       </div>
-      {/* <div className=""> */}
-        <div className="flex flex-col mt-[30px] mb-[30px] gap-[9px]">
-          {data.certification.map((data) => (
-            <div key={data.id} className="flex items-center gap-4">
-              <img src="/icons/certification_svg.svg" alt="certification" />
-              <p className="text-[12px] font-bold font-Nunito">{data.name}</p>
-            </div>
-          ))}
-        </div>
-      {/* </div> */}
+      <div className="flex flex-col mt-[30px] mb-[30px] gap-[9px]">
+        {data.certification.map((data) => (
+          <div key={data.id} className="flex items-center gap-4">
+            <img src="/icons/certification_svg.svg" alt="certification" />
+            <p className="text-[12px] font-bold font-Nunito">{data.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
