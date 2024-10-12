066c7d82b56c74fd170a48d38590e7a7440be953
tornike2002 <toko.toka2000@gmail.com>
2024-10-12 17:53:09 +0400

    fixed routing

--- src/components/admin/AdminLogin.jsx
+++ src/components/admin/AdminLogin.jsx
@@ -29,7 +29,7 @@ const AdminLogin = ({ onLogin }) => {
 
       localStorage.setItem("adminCredentials", encryptedCredentials);
       onLogin();
-      navigate("/dashboard");
+      navigate("/admin/dashboard");
     } else {
       setLoginError("Incorrect username or password.");
     }
