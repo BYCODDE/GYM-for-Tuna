3aca93080a83e90ce4d570d43e37f4fe702d5d49
BYCODDE <anaraliev217@gmail.com>
2024-10-16 17:29:05 +0400

    style:swiper style fixed!

--- src/components/stories/Certification.jsx
+++ src/components/stories/Certification.jsx
@@ -1,5 +1,4 @@
 import useGetCertification from "../../hooks/useGetCertification";
-import Arrows from "../../ui/Arrows";
 import ErorrDisplay from "../erorr/ErorrDisplay";
 import CertificationSkeleton from "../skeletons/CertificationSkeleton";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -15,18 +14,17 @@ function Certification() {
   }
 
   if (error) {
-    return <ErorrDisplay error={error.message} />;
+    return (
+      <ErorrDisplay error={error.message || "An unknown error occurred."} />
+    );
   }
 
   return (
-    <div className="mt-[10px] pb-10 md:max-w-[1281px] md:mx-auto 2xl:px-0 text-white px-[37px] flex flex-col justify-center">
-      <div className="md:gap-[15px] flex gap-[9px] items-center ml-[10px]">
-        <Arrows />
-        <h3 className="mt-[8px] text-center xl:text-[32px] text-sm font-bold uppercase leading-normal gradient-main pb-4 md:text-2xl font-Nunito">
-          certification
-        </h3>
-      </div>
-      <div className="mt-[30px] mb-[30px]">
+    <div className="mt-10 pb-10 md:max-w-[1281px] md:mx-auto 2xl:px-0 text-white px-10">
+      <h3 className="mt-8 text-center xl:text-[32px] text-sm font-bold uppercase leading-normal gradient-main pb-4 md:text-2xl font-Nunito">
+        Certification
+      </h3>
+      <div className="mt-8 mb-8 cursor-pointer">
         <Swiper
           modules={[Scrollbar]}
           scrollbar={{
@@ -36,11 +34,14 @@ function Certification() {
           }}
           spaceBetween={30}
           slidesPerView="auto"
-          className="relative scrollbar-thin scrollbar-thumb-scrollbarThumb scrollbar-track-scrollbarTrack"
+          className="scrollbar-thin scrollbar-thumb-scrollbarThumb scrollbar-track-scrollbarTrack"
         >
           {data.certification.map((cert) => (
-            <SwiperSlide key={cert.id} className="flex items-center gap-4 ">
-              <img src="/icons/certification_svg.svg" alt="certification" />
+            <SwiperSlide key={cert.id} className="flex items-center gap-4 p-3">
+              <img
+                src="/icons/certification_svg.svg"
+                alt={`Certification for ${cert.name}`}
+              />
               <p className="md:text-[14px] text-[12px] font-bold font-Nunito">
                 {cert.name}
               </p>
