6caf93c1d3738ce1f3db1b5c21d4feda037b23ff
BYCODDE <anaraliev217@gmail.com>
2024-11-15 14:17:08 +0400

    style:mobile version is done!

--- src/pages/admin-pages/AboutMe.jsx
+++ src/pages/admin-pages/AboutMe.jsx
@@ -1,9 +1,189 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const AboutMe = () => {
-  return (
-    <div>Admin Prices</div>
-  )
-}
+  const [imagePreview, setImagePreview] = useState(null); // For image preview
 
-export default AboutMe
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result); // Set image preview
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const onSubmit = (data) => {
+    console.log(data); // Submit form data
+    reset(); // Reset form after submission
+  };
+
+  return (
+    <div className="lg:p-[82px] text-[#FFF] font-Nunito p-[22px]">
+      <div className="flex items-center justify-between font-bold">
+        <h3 className="text-[22px] md:hidden flex">About Me</h3>
+        <div className="md:block hidden">
+          <h3 className="text-[22px]">About Me</h3>
+          <p className="text-[15px] font-normal">Add info for your clients</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-between items-center mt-[30px]">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="text-white w-full text-center mt-[41px] gap-[3.25rem] flex flex-col"
+        >
+          <div className="flex flex-col gap-[50px]">
+            <div className="w-full">
+              <h3 className="flex items-center mb-[20px] gap-[10px]">
+                Input your name
+              </h3>
+              <input
+                className="w-full focus:outline-none focus:border-none flex p-[10px] items-center rounded-[8px] bg-[#323232]"
+                type="text"
+                {...register("name", {
+                  required: "Name is required",
+                  pattern: {
+                    value: /^[a-z\s-]+$/i,
+                    message: "Only letters, spaces, and hyphens are allowed",
+                  },
+                  setValueAs: (value) => value.toLowerCase(),
+                })}
+              />
+              {errors.name && (
+                <div className="flex mt-[10px] font-bold text-red-500">
+                  {errors.name.message}
+                </div>
+              )}
+            </div>
+
+            <div className="w-full">
+              <h3 className="flex items-center mb-[20px] gap-[10px]">
+                Professional Title
+              </h3>
+              <input
+                className="w-full focus:outline-none focus:border-none flex p-[10px] items-center rounded-[8px] bg-[#323232]"
+                type="text"
+                {...register("title", {
+                  required: "Professional title is required",
+                })}
+              />
+              {errors.title && (
+                <div className="flex mt-[10px] font-bold text-red-500">
+                  {errors.title.message}
+                </div>
+              )}
+            </div>
+
+            <div className="w-full">
+              <h3 className="flex items-center mb-[20px] gap-[10px]">
+                Location
+              </h3>
+              <input
+                className="w-full focus:outline-none focus:border-none flex p-[10px] items-center rounded-[8px] bg-[#323232]"
+                type="text"
+                {...register("location", {
+                  required: "Location is required",
+                })}
+              />
+              {errors.location && (
+                <div className="flex mt-[10px] font-bold text-red-500">
+                  {errors.location.message}
+                </div>
+              )}
+            </div>
+
+            <div className="w-full">
+              <h3 className="flex items-center mb-[20px] gap-[10px]">
+                Contact
+              </h3>
+              <input
+                className="w-full focus:outline-none focus:border-none flex p-[10px] items-center rounded-[8px] bg-[#323232]"
+                type="text"
+                {...register("contact", {
+                  required: "Contact information is required",
+                  pattern: {
+                    value: /^[0-9]+$/,
+                    message: "Only numbers are allowed for contact information",
+                  },
+                })}
+              />
+              {errors.contact && (
+                <div className="flex mt-[10px] font-bold text-red-500">
+                  {errors.contact.message}
+                </div>
+              )}
+            </div>
+
+            <div className="w-full">
+              <h3 className="flex items-center mb-[20px] gap-[10px]">
+                Share your story
+              </h3>
+              <textarea
+                className="w-full h-[150px] focus:outline-none focus:border-none flex p-[10px] items-start rounded-[8px] bg-[#323232]"
+                {...register("story", {
+                  required: "Please share your story",
+                })}
+              />
+              {errors.story && (
+                <div className="flex mt-[10px] font-bold text-red-500">
+                  {errors.story.message}
+                </div>
+              )}
+            </div>
+
+            <div className="w-full">
+              <h3 className="flex items-center mb-[20px] gap-[10px]">
+                Upload Image
+              </h3>
+              <input
+                type="file"
+                accept="image/*"
+                className="w-full focus:outline-none focus:border-none flex p-[10px] items-center rounded-[8px] bg-[#323232]"
+                {...register("image", {
+                  required: "Image is required",
+                })}
+                onChange={handleImageChange}
+              />
+              {errors.image && (
+                <div className="flex mt-[10px] font-bold text-red-500">
+                  {errors.image.message}
+                </div>
+              )}
+
+              {imagePreview && (
+                <div className="mt-[20px]">
+                  <img
+                    src={imagePreview}
+                    alt="Selected"
+                    className="w-full h-auto rounded-[8px]"
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center">
+            <button
+              className="max-w-[195px] w-full text-[black] h-[42px] bg-[#D7FD44] rounded-[24px] font-bold"
+              type="submit"
+            >
+              Update Profile
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
